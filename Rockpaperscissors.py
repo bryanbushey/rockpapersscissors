12a5c0ed96695c3c4c9709fd29230e3031dfdf18
bryanbushey <55750447+bryanbushey@users.noreply.github.com>
2023-01-30 17:09:11 -0500

    Update

--- Rockpaperscissors.py
+++ Rockpaperscissors.py
@@ -1,49 +1,49 @@
 import random
+moves = ['rock','paper','scissors']
 def main():
-
-    games = int(input("How many games do you want to play?\n"))
-    for i in range(1,games+1):
-      user = 1
-      bot = 1
-      print("Game",i,"of",games)
-      while user == bot:
-        bot = random.randint(1,3)
-        user = input('Enter rock, paper, or scissors\n')
-        print()
-        if bot == 1 and user == 'paper':
-          print('You win!')
-        elif bot == 1 and user == 'scissors':
-          print('Computer wins!')
-        elif bot == 2 and user == 'rock':
-          print('Computer wins!')
-        elif bot == 2 and user == 'scissors':
-          print('You win!')
-        elif bot == 3 and user == 'rock':
-          print('You win!')
-        elif bot == 3 and user == 'paper':
-          print('Computer wins!')
-        elif bot == 1 and user == 'rock':
-          user = 1
-          bot = 1
-          print('You both chose rock!')
-          print()
-        elif bot == 2 and user == 'paper':
-          user = 1
-          bot = 1
-          print('You both chose paper!')
-          print()
+    keepGoingMain = 1
+    while keepGoingMain == 1:
+        computerMove = moves[random.randint(0,2)] #computer makes its own move on rage from 0 to 2
+        playerMove = getPlayerMove()
+        print('You chose:',playerMove,'\nPC chose:',computerMove)
+        if(playerMove == computerMove):
+            print('You both chose',playerMove,'play again!\n--------------------------\n')
+        elif(playerMove == 'paper') and (computerMove == 'rock'):
+            print('You Win!')
+            break
+        elif(playerMove == 'rock') and (computerMove == 'scissors'):
+            print('You Win!')
+            break
+        elif(playerMove == 'scissors') and (computerMove == 'paper'):
+            print('You Win!')
+            break
         else:
-          user = 1
-          bot = 1
-          print('You both chose scissors!')
-          print()
-      if bot == 1:
-        print('Computer chose rock.')
-      elif bot == 2:
-        print('Computer chose paper.')
-      else:
-        print('Computer chose Scissors.')
-      print()
-      print('------------------------------------------')
-    print('All done.')
-main()
+            print("You lose! You suck.")
+            break
+
+def getPlayerMove():
+    #creates escape route
+    while keepGoing == 1:
+        playerMove = input('Enter Rock, Paper, or Scissors: ').lower() #makes all inputs lower case
+        for i in range(0,3): #makes a loop with i incrementing every loop
+            if playerMove == moves[i]: #checks the address of the moves list with the players input
+                keepGoing = 0 #zero escapses the while loop
+    return(playerMove)
+
+def startPlaying():
+    while True:
+        try:
+            playTime = int(input("How many games do you want to play?\nEnter Here:"))
+            if not (type(playTime) is int):
+                raise ValueError()
+        except ValueError:
+            print("Invalid Option. Numbers only\n-----------------")
+        else:
+            break
+
+    for i in range(1,playTime+1):
+        main()
+        print('--------------------------')
+    print('Thank you for playing!')
+
+startPlaying()
